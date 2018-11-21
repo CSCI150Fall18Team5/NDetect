e98d07f14211fbf3a961f01d2acb9af4aa622980
ivanvasquez1967 <36181181+ivanvasquez1967@users.noreply.github.com>
2018-11-21 13:11:16 -0800

    Classes Outline
    Created a basic outine of functions for the Console

---
+++ NDetect/Console.cpp
@@ -0,0 +1,6 @@
+#include "pch.h";
+#include "Console.h";
+
+Console::Console() {
+}
+
---
+++ NDetect/Console.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "pch.h";
+class Console{
+	//Constructor and Destructor
+	Console();
+	~Console();
+
+	//The Five different types of Console options
+	void Console_Display();
+	void Live_Stream();
+	void Statistics();
+	void Combo_Diplay();
+	void Connections();
+
+
+};
