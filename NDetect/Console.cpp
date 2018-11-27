d952bcd3719a0d8e317b43f5bdcd050babca0ec7
ivanvasquez1967 <36181181+ivanvasquez1967@users.noreply.github.com>
2018-11-26 18:58:54 -0800

    Update Console.cpp
    Some minor changes to Statistics function.

--- NDetect/Console.cpp
+++ NDetect/Console.cpp
@@ -4,3 +4,31 @@
 Console::Console() {
 }
 
+Console::~Console() {
+
+}
+
+void Console::Console_Display(){
+
+}
+void Console::Live_Stream() {
+
+}
+void Console::Statistics() {
+	//Will call the class Statistics that will display the data. 
+	//Statistics Stats ();
+	//data=Stats.getData; 
+	/*
+		for(int i=0; i<data.size();i++){
+		printf("Statitcs data"+ data[i];
+		}
+	*/
+}
+void Console::Combo_Diplay() {
+	Live_Stream();
+	//Statistics();
+
+}
+void Console::Connections() {
+
+}
